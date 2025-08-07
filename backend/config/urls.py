819d7f9058dca467f43651aa86ce10ecb1c1bccf
beomseok <qjazk0000@gmail.com>
2025-08-07 11:15:04 +0900

    [추가] ACC02-FFU-01 이디이 비밀번호 입력 필드 구현

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import serve_react
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chatbot.urls')),
     path('api/accounts/', include('accounts.urls')),
+    
+    # React 앱을 루트 경로에서 서빙
+    re_path(r'^(?!admin/|api/|static/|media/).*$', serve_react, name='react_app'),
 ]
 
 if settings.DEBUG:
