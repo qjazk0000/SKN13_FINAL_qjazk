43ebb509e5d78ea6eaf4dc1664cee1ba18e1964c
dongwook <boogiewooki02@gmail.com>
2025-08-08 15:43:41 +0900

    feat: 리액트 초기 세팅 및 tailwindcss 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import MyPage from './pages/MyPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import MyPage from "./pages/MyPage/MyPage";
+import Chat from "./pages/Chat/Chat";
+import PasswordChange from "./pages/PasswordChange/PasswordChange";
+import "./App.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/change-password" element={<PasswordChange />} />
       </Routes>
     </Router>
   );
