0592c03e8d38f207594cb3bb210ef8141f55779f
boooms <qjazk0000@gmail.com>
2025-08-19 17:55:06 +0900

    feat:영수증 관리 페이지 조건 검색 구현

--- backend/adminapp/urls.py
+++ backend/adminapp/urls.py
@@ -1,8 +1,9 @@
 # adminapp/urls.py
 
 from django.urls import path
-from .views import AdminUsersView
+from .views import AdminUsersView, AdminReceiptsView
 
 urlpatterns = [
     path('users/', AdminUsersView.as_view(), name='admin-users'),
+    path('receipts/list/', AdminReceiptsView.as_view(), name='admin-receipts'),
 ]
