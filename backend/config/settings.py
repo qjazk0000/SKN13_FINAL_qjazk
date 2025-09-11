fb7d08418af0d1dc908da70c24abe27832b9b1b9
Ubuntu <ubuntu@ip-172-31-60-214.ap-northeast-2.compute.internal>
2025-09-11 08:15:27 +0000

    chore: setting.py 환경변수 수정

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1,43.200.226.184,ec2-43-200-226-184.ap-northeast-2.compute.amazonaws.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',['localhost', '127.0.0.1', '43.200.226.184', 'ec2-43-200-226-184.ap-northeast-2.compute.amazonaws.com']).split(',')
 
 # Application definition
 INSTALLED_APPS = [
